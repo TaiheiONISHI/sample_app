010b874a6966890c1a5ae7c9e5b9fe2c920b3c7e
Taihei_ONISHI <ridinglads@gmail.com>
2016-09-06 13:00:33 +0900

    7.4.4 成功時のテスト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
+
   def show
-      @user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def new
@@ -8,12 +9,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      # Handle a successful save.
-    else
-      render 'new'
-    end
+      @user = User.new(user_params)
+      if @user.save
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
+      else
+        render 'new'
+      end
   end
 
   private
