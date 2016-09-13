712a7aa319afee870a8ef6a6457e96d4672a3a20
Taihei_ONISHI <ridinglads@gmail.com>
2016-09-13 20:48:37 +0900

    9.1.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @user.update_attribute(user_params)
+    if @user.update_attributes(user_params)
       # 更新に成功したときの処理
     else
       render 'edit'
