255352d2c414825ee61677da04d051e397e6dba3
Taihei_ONISHI <ridinglads@gmail.com>
2016-09-13 19:38:31 +0900

    8.4.6

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,14 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
 
   # 与えられた文字列のハッシュ値を返す
-  def self.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
   # ランダムなトークンを返す
-  def self.new_token
+  def User.new_token
     SecureRandom.urlsafe_base64
   end
 
